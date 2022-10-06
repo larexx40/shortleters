8783c67e7bd8a4c89731ced12e9cc40966500288
Korex97 <egbeolowoakorede97@gmail.com>
2022-10-06 17:34:58 +0100

    Updates on the shortleters  platform

--- api/apartments/user_get_all_apartments.php
+++ api/apartments/user_get_all_apartments.php
@@ -173,6 +173,7 @@
                     $num_row = $result->num_rows;
                     
                 }
+
                 if ( $country_sort && !$sort_guest && $date_sort && $price_sort ){
                     $query = "SELECT apartments.* FROM apartments LEFT JOIN bookings ON bookings.apartment_id = apartments.apartment_id LEFT JOIN apartment_category ON apartment_category.category_id = apartments.category_id WHERE apartments.apartment_status = ? AND bookings.preferred_check_in != ? AND bookings.preferred_check_in != ? AND (apartments.price BETWEEN ? AND ?) AND apartments.apartment_country = ?  AND ( apartments.name LIKE ? OR apartment_category.name LIKE ? OR apartments.title LIKE ? OR apartments.description LIKE ? OR apartments.apartment_address LIKE ? OR apartments.apartment_lga LIKE ? OR apartments.apartment_city LIKE ? OR apartments.apartment_state LIKE ?) ORDER BY apartments.id DESC, apartments.feature DESC";
                     $queryStmt = $connect->prepare($query);
@@ -190,6 +191,7 @@
                     $num_row = $result->num_rows;
                     
                 }
+
                 if ( !$country_sort && $sort_guest && $date_sort && $price_sort ){
 
                     $query = "SELECT apartments.* FROM apartments LEFT JOIN bookings ON bookings.apartment_id = apartments.apartment_id LEFT JOIN apartment_category ON apartment_category.category_id = apartments.category_id WHERE apartments.apartment_status = ? AND bookings.preferred_check_in != ? AND bookings.preferred_check_in != ? AND (apartments.price BETWEEN ? AND ?)  AND apartments.max_guest <= ? AND ( apartments.name LIKE ? OR apartment_category.name LIKE ? OR apartments.title LIKE ? OR apartments.description LIKE ? OR apartments.apartment_address LIKE ? OR apartments.apartment_lga LIKE ? OR apartments.apartment_city LIKE ? OR apartments.apartment_state LIKE ?) ORDER BY apartments.id DESC, apartments.feature DESC";
@@ -247,6 +249,7 @@
                     $num_row = $result->num_rows;
                     
                 }
+                
                 if ( !$country_sort && $sort_guest && $date_sort && !$price_sort ){
 
                     $query = "SELECT apartments.* FROM apartments LEFT JOIN bookings ON bookings.apartment_id = apartments.apartment_id LEFT JOIN apartment_category ON apartment_category.category_id = apartments.category_id WHERE apartments.apartment_status = ? AND bookings.preferred_check_in != ? AND bookings.preferred_check_in != ? AND apartments.max_guest <= ? AND ( apartments.name LIKE ? OR apartment_category.name LIKE ? OR apartments.title LIKE ? OR apartments.description LIKE ? OR apartments.apartment_address LIKE ? OR apartments.apartment_lga LIKE ? OR apartments.apartment_city LIKE ? OR apartments.apartment_state LIKE ?) ORDER BY apartments.id DESC, apartments.feature DESC";
---
+++ api/review/addReview.php
@@ -0,0 +1,138 @@
+<?php
+    // pass cors header to allow from cross-origin
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");// OPTIONS,GET,POST,PUT,DELETE
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    Header("Cache-Control: no-cache");
+
+    include "../cartsfunction.php";
+    
+  
+
+    $endpoint = basename($_SERVER['PHP_SELF']);
+    $method = getenv('REQUEST_METHOD');
+
+    if ($method == 'POST') {
+
+        // Get company private key
+        $query = 'SELECT * FROM apidatatable';
+        $stmt = $connect->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row =  mysqli_fetch_assoc($result);
+        $companykey = $row['privatekey'];
+        $servername = $row['servername'];
+        $expiresIn = $row['tokenexpiremin'];
+
+        $decodedToken = ValidateAPITokenSentIN($servername, $companykey, $method, $endpoint);
+        $user_pubkey = $decodedToken->usertoken;
+
+        $user =  getUserWithPubKey($connect, $user_pubkey);
+
+        // send error if ur is not in the database
+        if (!$user){
+            // send user not found response to the user
+            $errordesc =  "User not Authorized";
+            $linktosolve = 'https://';
+            $hint = "User is not in the database ensure the user is in the database";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondUnAuthorized($data);
+        }
+
+        // Check if the recipient name field is passed
+        if (!isset($_POST['email'])){
+            $errordesc = "All fields must be passed";
+            $linktosolve = 'https://';
+            $hint = "Kindly pass the required email field in this endpoint";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondBadRequest($data);
+        }else{
+            $email = cleanme($_POST['email']);
+        }
+
+        if (!isset($_POST['apartment_id'])){
+            $errordesc = "All fields must be passed";
+            $linktosolve = 'https://';
+            $hint = "Kindly pass the required apartment id field in this endpoint";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondBadRequest($data);
+        }else{
+            $apartment_id = cleanme($_POST['apartment_id']);
+        }
+
+        if (!isset($_POST['review'])){
+            $errordesc = "All fields must be passed";
+            $linktosolve = 'https://';
+            $hint = "Kindly pass the required review field in this endpoint";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondBadRequest($data);
+        }else{
+            $email = cleanme($_POST['review']);
+        }
+
+        if (!isset($_POST['rate_star'])){
+            $errordesc = "All fields must be passed";
+            $linktosolve = 'https://';
+            $hint = "Kindly pass the required rate star field in this endpoint";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondBadRequest($data);
+        }else{
+            $rate_star = cleanme($_POST['rate_star']);
+        }
+   
+        
+         // check if none of the field is empty
+        if ( empty($email) || empty($review) || empty($apartment_id) || !is_numeric($rate_star) ){
+
+            $errordesc = "Insert all fields";
+            $linktosolve = 'https://';
+            $hint = "Kindly pass value to all the fields";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondBadRequest($data);
+        }
+
+
+        $review_id = generateUniqueShortKey($connect, "apartment_review", "review_id");
+
+
+        $query = 'INSERT INTO `apartment_review`(`review_id`, `email`, `userid`, `apartment_id`, `review`, `ratestar`) VALUES (?, ?, ?, ?, ?, ?)';
+        $slider_stmt = $connect->prepare($query);
+        $slider_stmt->bind_param("ssssss", $review_id, $email, $user, $apartment_id, $review, $rate_star);
+
+        if ( $slider_stmt->execute() ) {
+            $text= "Review successfully Sent";
+            $status = true;
+            $data = [];
+            $successData = returnSuccessArray($text, $method, $endpoint, [], $data, $status);
+            respondOK($successData);
+
+        }else{
+            $errordesc =  $slider_stmt->error;
+            $linktosolve = 'https://';
+            $hint = "500 code internal error, check ur database connections";
+            $errorData = returnError7003($errordesc, $linktosolve, $hint);
+            $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+            respondInternalError($data);
+        }
+
+    }else{
+
+        // Send an error response because a wrong method was passed 
+        $errordesc = "Method not allowed";
+        $linktosolve = 'https://';
+        $hint = "This route only accepts POST request, kindly pass a post request";
+        $errorData = returnError7003($errordesc, $linktosolve, $hint);
+        $data = returnErrorArray($errordesc, $method, $endpoint, $errorData, []);
+        respondMethodNotAlowed($data);
+        
+    }
+
+
+?>
